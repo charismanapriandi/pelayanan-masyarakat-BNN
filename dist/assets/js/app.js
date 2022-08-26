d26179c029ffe4b95b92d949e609c258cf4b1a97
Risman <rismantrdr@gmail.com>
2022-08-26 22:48:06 +0700

    add success view

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -70,6 +70,12 @@
   })
 })();
 
-// $(document).ready(function () {
-//   alert('navigation')
-// })
+(() => {
+  // const inputNode = document.querySelectorAll('input[type="file"')
+
+  // for (let input of inputNode) {
+  //   input.addEventListener('change', (e) => {
+  //     console.log(e)
+  //   })
+  // }
+})()
---
+++ src/js/upload-file.js
@@ -0,0 +1,9 @@
+(() => {
+  // const inputNode = document.querySelectorAll('input[type="file"')
+
+  // for (let input of inputNode) {
+  //   input.addEventListener('change', (e) => {
+  //     console.log(e)
+  //   })
+  // }
+})()
